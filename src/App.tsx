a2b2731dc296eb9c5838ce541fffa6396daf559a
virat18543 <virattendulkar1854@gmail.com>
2025-08-25 18:27:20 +0530

    Remove Downloads route and import from App.tsxUpdate App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-﻿import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -13,7 +13,6 @@ import Applications from "./pages/Applications";
 import PhilosophyPage from "./pages/PhilosophyPage";
 import Resources from "./pages/Resources";
 import ContactPage from "./pages/ContactPage";
-import Downloads from "./pages/Downloads";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import AsaVsPetg from "./pages/blog/AsaVsPetg";
 import SustainableDesign from "./pages/blog/SustainableDesign";
@@ -45,12 +44,11 @@ const App = () => (
             <Route path="/philosophy" element={<PhilosophyPage />} />
             <Route path="/resources" element={<Resources />} />
             <Route path="/contact" element={<ContactPage />} />
-        <Route path="/downloads" element={<Downloads />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/blog/asa-vs-petg" element={<AsaVsPetg />} />
-        <Route path="/blog/sustainable-design" element={<SustainableDesign />} />
-        <Route path="/blog/louver-systems" element={<LouverSystems />} />
-        <Route path="*" element={<NotFound />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/blog/asa-vs-petg" element={<AsaVsPetg />} />
+            <Route path="/blog/sustainable-design" element={<SustainableDesign />} />
+            <Route path="/blog/louver-systems" element={<LouverSystems />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
           <BackToTop />
@@ -62,5 +60,3 @@ const App = () => (
 );
 
 export default App;
-
-
