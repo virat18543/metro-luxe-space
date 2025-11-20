bc1d95598596017b3b6f3c6c02f598a0240b9abf
virat18543 <virattendulkar1854@gmail.com>
2025-11-20 15:44:34 +0530

    Create Acrycore component with greeting message

---
+++ src/pages/Acrycore.tsx
@@ -0,0 +1,11 @@
+const Acrycore = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Hi, see you soon</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Acrycore;
